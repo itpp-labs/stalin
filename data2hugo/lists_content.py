3da0b7f87984ed6ee43d42054a78f4c4d302d513
Ivan Yelizariev <yelizariev@itpp.dev>
2020-07-03 05:16:34 +0000

    lists: add opengraph meta data

--- data2hugo/lists_content.py
+++ data2hugo/lists_content.py
@@ -2,11 +2,17 @@ from common import *
 
 def main():
     for lst in csv_reader(LISTS_CSV):
-        content = """
----
-title: "{title}"
----
-""".format(title=list2title(lst).replace('"', '\\"'))
+        data_file = os.path.join(HUGO_DATA_DIR, "lists", "list%s.yaml" % lst["listid"])
+        data = yaml_reader(data_file)
+        meta = {
+            "title": data["title"],
+            "description": "%s, лист %s" % (data["archive"]["ref"], data["archive"]["page1"]),
+            "images": [
+                "/disk/pictures/%s" % p["image"]
+                for p in data["pages"][:6]
+            ]
+        }
+        content = "---\n%s---" % yaml.dump(meta, allow_unicode=True)
         file_writer(
             os.path.join(HUGO_CONTENT_DIR, "lists", "%s.md" % list2name(lst)),
             content
