3db5ead0cf61c507fe362a32e7b407bd98e8580b
Ivan Yelizariev <yelizariev@itpp.dev>
2020-07-03 05:35:37 +0000

    person: add meta description for opengraph

--- data2hugo/persons_content.py
+++ data2hugo/persons_content.py
@@ -2,10 +2,24 @@ from common import *
 
 def main():
     for p in csv_reader(PERSONS_CSV):
-        content = """---
-title: {nameshow1}
----
-""".format(**p)
+        if p["personid"] != p["headperson"]:
+            # it's a technical record for a person from another record
+            continue
+
+        data_file = os.path.join(HUGO_DATA_DIR, "persons", "p%s.yaml" % p["personid"])
+        data = yaml_reader(data_file)
+        spravki_file = os.path.join(HUGO_DATA_DIR, "spravki", "p%s.yaml" % p["personid"])
+        spravki = []
+        try:
+            spravki = yaml_reader(spravki_file)
+        except:
+            pass
+        meta = {
+            "title": data["name"]["nameshow"],
+        }
+        if spravki:
+            meta["description"] = spravki[0]["spravka"]
+        content = "---\n%s---" % yaml.dump(meta, allow_unicode=True)
         file_writer(
             os.path.join(HUGO_CONTENT_DIR, "persons", "%s.md" % person2name(p)),
             content
