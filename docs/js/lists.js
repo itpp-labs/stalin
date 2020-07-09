4855ed671b36d5b4cbb0955bb39b1b45c4884d38
yelizariev <yelizariev@users.noreply.github.com>
2020-07-09 11:10:45 +0000

    :alien: make website
    Sent from Github Actions (see .github/workflows/make.yml )

---
+++ docs/js/lists.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    if (!$(".pages").length){
+        // we are on lists page
+        return;
+    }
+    slider.events.on("indexChanged", function(event){
+        var item = event.slideItems[event.index];
+        var id = $(item).attr("id");
+        if (id)
+            history.pushState(null,null,'#' + id);
+    });
+
+    $('.pages img').on('click', function(e){
+        var pos = (e.pageX - $(this).offset().left) / $(this).width();
+        console.log(pos);
+        if (pos < 0.25)
+            slider.goTo("prev");
+        else
+            slider.goTo("next");
+    });
+
+
+    $('.page-image').on('click', function() {
+        $page = $(this).parent();
+        $page.find('.page-image, .page-text').toggleClass('is-hidden');
+    });
+
+})
